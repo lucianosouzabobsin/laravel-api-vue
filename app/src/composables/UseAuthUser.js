7ea7141f2c2c1c1e7de31040b8933bcc53ad06d8
Luciano Bobsin <lucianosouzabobsin@gmail.com>
2023-11-20 01:07:29 -0300

    Criado datatable personalizado com endpoints

--- app/src/composables/UseAuthUser.js
+++ app/src/composables/UseAuthUser.js
@@ -1,20 +1,23 @@
 import { ref } from 'vue'
 import { api } from 'boot/axios'
 import { LocalStorage } from 'quasar'
+import { useRouter } from 'vue-router'
 
 const user = ref(null)
 
 export default function useAuthUser () {
 
+  const router = useRouter()
+
   const login = async ({ email, password }) => {
-    LocalStorage.set('userData', null)
+    LocalStorage.set('userData', null);
 
     try {
         const response = await api.post('http://localhost/api/login', { email, password });
         const token = response.data.token;
         const userData = await getUser(token);
 
-        LocalStorage.set('userData', userData)
+        LocalStorage.set('userData', userData);
 
     } catch (error) {
       if (typeof error.response !== 'undefined') {
@@ -49,11 +52,23 @@ export default function useAuthUser () {
     LocalStorage.set('userData', null)
   }
 
-  const isLoggedIn = () => {
+  const isLoggedIn = async () => {
     const userData = LocalStorage.getItem('userData');
 
     if (!userData || userData.name === undefined) {
       return false;
+    }
+
+    try {
+      const response = await getUser(userData.token);
+    } catch (error) {
+      if (typeof error.response !== 'undefined') {
+
+          if (error.response.statusText == 'Unauthorized') {
+            console.log(error.response.statusText);
+            router.push({ name: 'login' });
+          }
+      }
     }
 
     return true;
